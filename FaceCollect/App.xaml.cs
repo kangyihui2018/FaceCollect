75108982e4a34b9bc8f181ef3013f6236c2ad3ce
康逸飞 <kyifei@qq.com>
2020-01-04 14:37:58 +0800

    。。

--- FaceCollect/App.xaml.cs
+++ FaceCollect/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using FaceCollect.Entity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+
 
 namespace FaceCollect
 {
@@ -13,5 +15,11 @@ namespace FaceCollect
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+           var temp= RasAssist.CallRemoteService<IFaceCollect, string[]>(ee=>ee.GetDepartments());
+            base.OnStartup(e);
+        }
     }
 }
