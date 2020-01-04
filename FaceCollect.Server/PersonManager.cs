3dad40f54bd77fe3a5e4e530698951759c8afb53
康逸飞 <kyifei@qq.com>
2020-01-04 10:49:36 +0800

    .

---
+++ FaceCollect.Server/PersonManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using FaceCollect.Entity;
+
+namespace FaceCollect.Server
+{
+    class PersonManager : FaceCollect.Entity.IFaceCollect
+    {
+        public bool AddOrEditPersonInfo(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ImageInfo> GetFace(int pageNo, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetFacePageCount(int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetPersonInfo(string department, string name, string phone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
